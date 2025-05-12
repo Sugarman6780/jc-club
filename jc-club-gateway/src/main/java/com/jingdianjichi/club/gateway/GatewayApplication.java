c29506601224b3fcb5a1c67f2dcd8f1420c7b8d9
子涵 徐 <2042108112@qq.com>
2025-05-12 17:14:14 +0800

    注册各服务到nacos

--- jc-club-gateway/src/main/java/com/jingdianjichi/club/gateway/GatewayApplication.java
+++ jc-club-gateway/src/main/java/com/jingdianjichi/club/gateway/GatewayApplication.java
@@ -1,15 +1,14 @@
-package com.jingdianjichi.auth;
+package com.jingdianjichi.club.gateway;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.jingdianjichi")
-@MapperScan("com.jingdianjichi.**.mapper")
-public class AuthApplication {
+public class GatewayApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(AuthApplication.class);
+        SpringApplication.run(GatewayApplication.class);
     }
 }
